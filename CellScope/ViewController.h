7cea74bdb3cb8375ffdfae546b1c31b94a06feb2
Matthew Bakalar <matthewbakalar@matbak87.local>
2012-03-11 20:55:28 -0700

    Initial Commit

---
+++ CellScope/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CellScope
+//
+//  Created by Matthew Bakalar on 3/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CellScopeTests/CellScopeTests.h
@@ -0,0 +1,13 @@
+//
+//  CellScopeTests.h
+//  CellScopeTests
+//
+//  Created by Matthew Bakalar on 3/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CellScopeTests : SenTestCase
+
+@end
